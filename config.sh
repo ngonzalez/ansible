7a689a843998d86b42eab1e459119c706d46e14f
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-05 14:50:51 +0200

    Update config

--- config.sh
+++ config.sh
@@ -1,9 +1,12 @@
-export NAMESPACE='ns'
-export PROJECT_NAME='project'
-export BUCKET_NAME='storage'
+# ansible
 export ACCOUNT='ngonzalez'
 export ANSIBLE_USER='debian'
 export ANSIBLE_PORT='22'
+
+# gcp
+export PROJECT_NAME='project'
+export NAMESPACE='namespace'
+export BUCKET_NAME='storage'
 export CLUSTER_NAME='app'
 export APP_CLUSTER_IP='APP_CLUSTER_IP'
 export DB_CLUSTER_IP='DB_CLUSTER_IP'
