d56898fb73c03ad46b83ecfdc18b0f16f8dbec0a
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-22 22:42:51 +0200

    Update config change namespace

--- config.sh
+++ config.sh
@@ -1,12 +1,13 @@
 # ansible
-export ACCOUNT='ngonzalez'
+export ACCOUNT='docker'
 export ANSIBLE_USER='debian'
 export ANSIBLE_PORT='22'
 
 # gcp
+export NAMESPACE='k8s'
 export PROJECT_NAME='project'
 export BUCKET_NAME='storage'
-export CLUSTER_NAME='app'
+export CLUSTER_NAME='db'
 export APP_CLUSTER_IP='APP_CLUSTER_IP'
 export DB_CLUSTER_IP='DB_CLUSTER_IP'
 export KIBANA_CLUSTER_IP='KIBANA_CLUSTER_IP'
