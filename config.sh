2bcc8d8acb2c12e47ef096609eec5793371f4a75
Nicolas Gonzalez <n.gonzalez@groupeonepoint.com>
2021-05-13 23:26:01 +0200

    Update config

--- config.sh
+++ config.sh
@@ -8,6 +8,5 @@ export NAMESPACE='k8s'
 export GCP_ENABLED='true'
 export PROJECT_NAME='app-56468'
 export BUCKET_NAME="$PROJECT_NAME-storage"
-export CLUSTER_NAME='app'
 export APP_CLUSTER_IP='10.110.185.251'
 export DB_CLUSTER_IP='10.99.7.128'
