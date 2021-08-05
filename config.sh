fea3c9989396da7e0bbd7be9f10336cb5af645f6
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-08-05 18:28:22 +0200

    Update app environment variables

--- config.sh
+++ config.sh
@@ -3,19 +3,10 @@ export ACCOUNT='docker'
 export ANSIBLE_USER='debian'
 export ANSIBLE_PORT='22'
 
-# k8s
+# kubernetes
 export NAMESPACE='k8s'
 export INGRESS_NAME='app-ingress'
 export DATABASE_LOADBALANCER='database-loadbalancer'
 
 # gcp
 export GCP_ENABLED='true'
-
-# app server
-export APP_SERVER_PORT="80"
-export APP_SERVER_PATH="/hls"
-
-# backup server
-export BACKUP_SERVER_HOST="192.168.1.13"
-export BACKUP_SERVER_PORT="8080"
-export BACKUP_SERVER_PATH="/Volumes/Samsung_T5"
