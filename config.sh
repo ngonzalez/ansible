fe094a27cc8b8fce9eba7cb919bb44967d706b7e
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-08-05 10:19:28 +0200

    Update config

--- config.sh
+++ config.sh
@@ -3,10 +3,19 @@ export ACCOUNT='docker'
 export ANSIBLE_USER='debian'
 export ANSIBLE_PORT='22'
 
-# gcp
+# k8s
 export NAMESPACE='k8s'
-export GCP_ENABLED='true'
-export PROJECT_NAME='app-56468'
-export BUCKET_NAME="$PROJECT_NAME-storage"
 export INGRESS_NAME='app-ingress'
 export DATABASE_LOADBALANCER='database-loadbalancer'
+
+# gcp
+export GCP_ENABLED='true'
+
+# app server
+export APP_SERVER_PORT="80"
+export APP_SERVER_PATH="/hls"
+
+# backup server
+export BACKUP_SERVER_HOST="192.168.1.13"
+export BACKUP_SERVER_PORT="8080"
+export BACKUP_SERVER_PATH="/Volumes/Samsung_T5"
