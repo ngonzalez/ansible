156c83f9b497a888ac878bd289e4c55c11f78f15
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-12-08 01:32:51 +0100

    Update config

--- config.sh
+++ config.sh
@@ -7,9 +7,6 @@ export INVENTORY_FILE='inventory.yml'
 
 # kubernetes
 export NAMESPACE='k8s'
-export APP_LOADBALANCER='app-loadbalancer'
-export DB_LOADBALANCER='database-loadbalancer'
-export LOADBALANCERS_INVENTORY_FILE='roles/app/vars/inventory.yml'
 
 # gcp
 export GCP_ENABLED='true'
