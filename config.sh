efdd35db1843643191ca4b474e25cbc5e07e0b17
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-11-06 17:24:29 +0100

    Update config, remove empty folder

--- config.sh
+++ config.sh
@@ -1,11 +1,2 @@
 # ansible
 export INVENTORY_FILE='inventory.yaml'
-
-# loadbalancers
-export NAMESPACE='k8s'
-export APP_LOADBALANCER='app-loadbalancer'
-export DATABASE_LOADBALANCER='database-loadbalancer'
-export LOADBALANCERS_INVENTORY_FILE='inventory/vars.yaml'
-
-# gcp
-export GCP_ENABLED='true'
